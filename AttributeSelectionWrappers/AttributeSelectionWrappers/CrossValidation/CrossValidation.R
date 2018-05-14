101f619f630f82a7913e176c4ec8a8cbcc0eecca
marskr <marskr@opoczta.pl>
2018-05-14 16:01:20 +0200

    rpart bug fix #1

--- AttributeSelectionWrappers/AttributeSelectionWrappers/CrossValidation/CrossValidation.R
+++ AttributeSelectionWrappers/AttributeSelectionWrappers/CrossValidation/CrossValidation.R
@@ -5,6 +5,7 @@ source("DataNormalization/DataEncryption.R")
 head(encData, 5)
 
 kfcv.sizes = function(n, k = 10) {
+
     # generate sample sizes for k-fold cross validation on a data set of
     # size n 
     # create sizes of provided data division into k folds
@@ -20,6 +21,7 @@ kfcv.sizes = function(n, k = 10) {
 }
 
 kfcv.testing = function(n, k = 10) {
+
     # generate testing sample indices for k-fold cross validation on a
     # data set of size n
     # EXAMPLE: kfcv.testing(n, k = ...)
@@ -38,7 +40,8 @@ kfcv.testing = function(n, k = 10) {
     indices
 }
 
-kfcv.classifier = function(data, class, classifier, k = 10) {
+kfcv.classifier = function(data, class, classTested = 1, classifier, k = 10) {
+
     # run k-fold cross validation with an arbitrary classifier
     # EXAMPLE: kfcv.classifier(data, class, classifier, k = ...)
     #
@@ -56,7 +59,7 @@ kfcv.classifier = function(data, class, classifier, k = 10) {
         test = data[testingindices,] # take all rows that are in testingindicates list
 
         #result[[i]] = classifier.naivebayes(train, test, class)
-        result[[i]] = classifier.decisiontree(train, test, class, testingindices)
+        result[[i]] = classifier.decisiontree(train, test, class, testingindices, classTested)
 
         err = 1.0 - kfcv.sum(result[[i]]) / sum(result[[i]])
         #print(result[[i]][1, 1] + result[[i]][2, 2]) / sum(result[[i]])
@@ -67,45 +70,52 @@ kfcv.classifier = function(data, class, classifier, k = 10) {
 }
 
 kfcv.sum = function(result) {
-    diagonalSum = 0 
+
+    diagonalSum = 0
     for (i in 1:(dim(result)[1])) {
         kfcv.increment(diagonalSum, result[i, i])
     }
-    diagonalSum    
+    diagonalSum
 }
 
 kfcv.increment = function(data, value) {
+
     eval.parent(substitute(data <- data + value))
 }
 
 classifier.naivebayes = function(train, test, class) {
+
     # simple example of a classifier
     # requires library(e1071)
     model = naiveBayes(train[, - class], train[, class])
     table(predict(model, test[, - class]), test[, class])
 }
 
-classifier.decisiontree = function(train, test, class, testindicates) {
+classifier.decisiontree = function(train, test, class, testindicates, classTested = 1) {
+
     # decision tree classifier
     # requires library(rpart)
-    modelTree = rpart(train[, class %% dim(test)[2]] ~ train[, (class + 2) %% dim(test)[2]], method = "class", data = train)
+    index = ((class + 1) %% dim(train)[2]) + 1
+
+    modelTree = rpart(train[, 1] ~ train[, class], method = "class", data = train) #%% dim(test)[2]
     testPred = predict(modelTree, newData = test, type = "class")
-    table(testPred[testindicates], test[, class %% dim(test)[2]])
+
+    table(testPred[testindicates], test[, class])
 }
 
 kfcv.error = function(data, n, k = 10) {
 
     all.err = numeric(0)
     for (i in 1:n) {
-        err = kfcv.classifier(data, i, classifier.naivebayes, 5)
+        err = kfcv.classifier(data, i, 2, classifier.decisiontree, 5)
         cat(err, " is the error of iteration ", i, "\n")
         if (err != 0)
             all.err = rbind(all.err, err)
-    }
+        }
     min(all.err)
 }
 
-kfcv.sizes(dim(encData), 5)
-kfcv.testing(dim(encData)[1], 5)
-kfcv.classifier(encData, 1, classifier.naivebayes, 5)
-kfcv.error(encData, 3, 5)
+#kfcv.sizes(dim(transformedData), 5)
+#kfcv.testing(dim(transformedData)[1], 5)
+#kfcv.classifier(transformedData, 1, classifier.naivebayes, 5)
+#kfcv.error(transformedData, 5, 5)
